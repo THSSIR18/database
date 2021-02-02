ceb4dc3a6e345874b5c1ed17cbbc08ba2db5be68
SAHIL RAJAPKAR <70615373+THSSIR18@users.noreply.github.com>
2021-02-01 23:57:33 -0500

    Add files via upload

---
+++ Project2_1.java
@@ -0,0 +1,18 @@
+/*
+	Myles Lesser
+	Project 2_1
+	April 22,2015
+*/
+public class Project2_1
+{
+	public static void main(String[] args)
+	{
+		String firstName="Myles";
+		String lastName="Lesser";
+		String adress="1009 Warren Parkway";
+		String num = "201-694-7609";
+		System.out.println(firstName + "" + lastName );
+		System.out.println(adress);
+		System.out.println(num);
+	}
+}
